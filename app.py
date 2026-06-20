0e6b28d0f71716a7723674385f475474c78990d5
Sysiuk Vladyslav <vladkrytuz@gmail.com>
2026-06-21 00:34:49 +0300

    Simplify app.py for better compatibility

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
-# Get the absolute path to the project directory
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__, 
-            static_folder='static',
-            static_url_path='/static',
-            template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory(os.path.join(basedir, 'static'), filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
